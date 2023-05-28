942f4d09216048796e9557f46d5919bc77ad448d
Shakir <30622440+networkandcode@users.noreply.github.com>
2023-05-28 06:08:35 +0000

    speak dot text

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,74 @@ import { useEffect, useState, useRef } from "react"
 
 const Home = () => {
   const msg = typeof window !== 'undefined' && new SpeechSynthesisUtterance()
+  if (msg) {
+    msg.rate = 0.8
+  }
 
   const [ brailleMode, setBrailleMode ] = useState(false)
   const [ brailleText, setBrailleText ] = useState('')
+  const [ dotText, setDotText ] = useState('')
   const [text, setText] = useState('')
   const [ textToRead, setTextToRead ] = useState('')
 
-  const textareaRef = useRef()
+  const textRef = useRef()
+  const brailleTextRef = useRef()
 
+  // generated with chatgpt
+  const character_dict = {
+    A: { dot: 'dot 1', braille: '⠁' },
+    B: { dot: 'dot 1 2', braille: '⠃' },
+    C: { dot: 'dot 1 4', braille: '⠉' },
+    D: { dot: 'dot 1 4 5', braille: '⠙' },
+    E: { dot: 'dot 1 5', braille: '⠑' },
+    F: { dot: 'dot 1 2 4', braille: '⠋' },
+    G: { dot: 'dot 1 2 4 5', braille: '⠛' },
+    H: { dot: 'dot 1 2 5', braille: '⠓' },
+    I: { dot: 'dot 2 4', braille: '⠊' },
+    J: { dot: 'dot 2 4 5', braille: '⠚' },
+    K: { dot: 'dot 1 3', braille: '⠅' },
+    L: { dot: 'dot 1 2 3', braille: '⠇' },
+    M: { dot: 'dot 1 3 4', braille: '⠍' },
+    N: { dot: 'dot 1 3 4 5', braille: '⠝' },
+    O: { dot: 'dot 1 3 5', braille: '⠕' },
+    P: { dot: 'dot 1 2 3 4', braille: '⠏' },
+    Q: { dot: 'dot 1 2 3 4 5', braille: '⠟' },
+    R: { dot: 'dot 1 2 3 5', braille: '⠗' },
+    S: { dot: 'dot 2 3 4', braille: '⠎' },
+    T: { dot: 'dot 2 3 4 5', braille: '⠞' },
+    U: { dot: 'dot 1 3 6', braille: '⠥' },
+    V: { dot: 'dot 1 2 3 6', braille: '⠧' },
+    W: { dot: 'dot 2 4 5 6', braille: '⠺' },
+    X: { dot: 'dot 1 3 4 6', braille: '⠭' },
+    Y: { dot: 'dot 1 3 4 5 6', braille: '⠽' },
+    Z: { dot: 'dot 1 3 5 6', braille: '⠵' },
+    '0': { dot: 'dot 2 4 5 5', braille: '⠼⠚' },
+    '1': { dot: 'dot 1', braille: '⠼⠁' },
+    '2': { dot: 'dot 1 2', braille: '⠼⠃' },
+    '3': { dot: 'dot 1 4', braille: '⠼⠉' },
+    '4': { dot: 'dot 1 4 5', braille: '⠼⠙' },
+    '5': { dot: 'dot 1 5', braille: '⠼⠑' },
+    '6': { dot: 'dot 1 2 4', braille: '⠼⠋' },
+    '7': { dot: 'dot 1 2 4 5', braille: '⠼⠛' },
+    '8': { dot: 'dot 1 2 5', braille: '⠼⠓' },
+    '9': { dot: 'dot 2 4 5 5', braille: '⠼⠚' },
+    '.': { dot: 'dot 3 4 6', braille: '⠲' },
+    ',': { dot: 'dot 3', braille: '⠂' },
+    '?': { dot: 'dot 2 3 6', braille: '⠢' },
+    '!': { dot: 'dot 1 2 3 5 6', braille: '⠖' },
+    "'": { dot: 'dot 2', braille: '⠄' },
+    '"': { dot: 'dot 2 3 5 6', braille: '⠶' },
+    '-': { dot: 'dot 2 5 6', braille: '⠤' },
+    '(': { dot: 'dot 2 3 6 6', braille: '⠐⠣' },
+    ')': { dot: 'dot 2 3 6 5 6', braille: '⠐⠜' },
+    '/': { dot: 'dot 1 2 4 6', braille: '⠌' },
+    '+': { dot: 'dot 1 2 3 5 6', braille: '⠖' },
+    '=': { dot: 'dot 2 3 5 6 6', braille: '⠐⠶' },
+    '#': { dot: 'dot 3 4 6 6', braille: '⠼⠲' },
+    '@': { dot: 'dot 2 3 6 5 6', braille: '⠜⠜' },
+    ';': { dot: 'dot 2 3 6', braille: '⠆' },
+  };    
+  
   const onChange = e => {
     e.preventDefault()
     const { value } = e.target
@@ -17,21 +77,30 @@ const Home = () => {
     setText(value)
 
     let linesInBraille = []
+    let linesInDot = []
 
     value.split('\n').map(lineInEnglish => {
       if (lineInEnglish === '') {
         linesInBraille = [ ...linesInBraille, lineInEnglish ]
+        linesInDot = [ ...linesInDot, lineInEnglish ]
       } else {
-        const lineInBraille = lineInEnglish
+        let lineInBraille = ''
+        let lineInDot = ''
+        lineInEnglish
           .toUpperCase()
           .split('')
-          // https://stackoverflow.com/questions/69756010/convert-string-to-braille
-          .map(char => "⠀⠁⠂⠃⠄⠅⠆⠇⠈⠉⠊⠋⠌⠍⠎⠏⠐⠑⠒⠓⠔⠕⠖⠗⠘⠙⠚⠛⠜⠝⠞⠟⠠⠡⠢⠣⠤⠥⠦⠧⠨⠩⠪⠫⠬⠭⠮⠯⠰⠱⠲⠳⠴⠵⠶⠷⠸⠹⠺⠻⠼⠽⠾⠿"[" A1B'K2L@CIF/MSP\"E3H9O6R^DJG>NTQ,*5<-U8V.%[$+X!&;:4\\0Z7(_?W]#Y)=".indexOf(char)]).join("")
-          linesInBraille = [ ...linesInBraille, lineInBraille ]
+          .forEach(char => {
+            char = char.toUpperCase()
+            lineInBraille += char === ' ' ? char : character_dict[char]['braille']
+            lineInDot += char === ' ' ? char : character_dict[char]['dot']
+          })
+        linesInBraille = [ ...linesInBraille, lineInBraille ]
+        linesInDot = [ ...linesInDot, lineInDot ]
       }
     })
 
     setBrailleText(linesInBraille.join('\n'))
+    setDotText(linesInDot.join('\n'))
 
     const lastChar = value.charAt(value.length - 1)
     if (lastChar === ' ') {
@@ -43,12 +112,16 @@ const Home = () => {
     }
   }
 
-  const onKeyDown = e => {
-    const { value } = e.target
-    if (e.shiftKey, e.key === 'T') {
+  const handleKeyPressInTextMode = e => {
+    if (e.ctrlKey || e.metaKey) {
       e.preventDefault()
-      setBrailleMode(!brailleMode)
+      if (e.code === 'Digit2') {
+        setBrailleMode(!brailleMode)
+      } else if (e.code === 'Digit3') {
+        setTextToRead(text)
+      }
     } else if (e.code.includes('Arrow')) {
+      const { value } = e.target
       const length = value.length
       let start = e.target.selectionStart
      
@@ -57,27 +130,25 @@ const Home = () => {
       } else if (e.code === 'ArrowRight') {
         start = start + 1
       }
-
-      console.log(start, length)
       
       if (start <= 0) {
         setTextToRead(`${value[0]}, beginning of the document`)
       } else if (start >= length) {
         setTextToRead('End of the document')
       } else {
-        setTextToRead(value[start])
-      }
-    } else if (e.ctrlKey) {
-      if (e.code === 'KeyA') {
-        setTextToRead(value)
+        setTextToRead(value[start].trim() || 'space')
       }
     }
   }
 
-  const onSelect = e => {
-    e.preventDefault()
-    if (typeof window !== 'undefined') {
-      setTextToRead(window.getSelection().toString())
+  const handleKeyPressInBrailleMode = e => {
+    if (e.ctrlKey || e.metaKey) {
+      e.preventDefault()
+      if (e.code === 'Digit1') {
+        setBrailleMode(!brailleMode)
+      } else if (e.code === 'Digit3') {
+        setTextToRead(dotText)
+      }
     }
   }
 
@@ -87,8 +158,14 @@ const Home = () => {
   }
 
   useEffect(() => {
-    textareaRef.current.focus()
-  },[])
+    if (brailleMode) {
+      brailleTextRef.current.focus()
+      setTextToRead('You are in braille view mode')
+    } else {
+      textRef.current.focus()
+      setTextToRead('You are in text edit mode')
+    }
+  },[ brailleMode ])
 
   useEffect(() => {
     msg.text = textToRead
@@ -108,8 +185,10 @@ const Home = () => {
           <textarea
             id='brailleText'
             name='brailleText'
+            onKeyDown={handleKeyPressInBrailleMode}
             placeholder='View in Braille'
             readOnly
+            ref={brailleTextRef}
             style={{ fontSize: `50px`, height: `100%`, lineHeight: `1`, overflow: `hidden`, width: `100%` }}
             value={brailleText}
           />
@@ -117,10 +196,10 @@ const Home = () => {
           <textarea
             id='text'
             onChange={onChange}
-            onKeyDown={onKeyDown}
+            onKeyDown={handleKeyPressInTextMode}
             name='text'
             placeholder='Type in English'
-            ref={textareaRef}
+            ref={textRef}
             style={{ fontSize: `50px`, height: `100%`, lineHeight: `1`, overflow: `hidden`, width: `100%` }}
             value={text}
           />
